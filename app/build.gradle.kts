3fa806fdba05f2e3d48fde7f92b38672f96c875a
Manthan.Aggarwal <Manthan.Aggarwal@ril.com>
2025-09-04 15:36:38 +0530

    Manthan Aggarwal -V.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,14 +15,24 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
